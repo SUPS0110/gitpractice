4aec6c39b7c916ef3f9c1d24e1011e07cc5de6c8
SUPS0110 <suprima0110@gmail.com>
2022-10-16 21:53:53 +0545

    random friendlist(using api)

--- portfolio/stylesheet/JS/index.js
+++ portfolio/stylesheet/JS/index.js
@@ -1,5 +1,36 @@
-let subMenu = document.getElementById("subMenu");
-function toggleMenu()
-{
-    subMenu.classList.toggle("open");
-}
+const table = document.querySelector("table");
+
+fetch('https://634c1296317dc96a3090b7a0.mockapi.io/insta')
+.then((res)=>res.json())
+.then((responseJson)=>{
+ responseJson.forEach((item)=>{
+  const tr = document.createElement("tr");
+const name = document.createElement("td");
+ const age = document.createElement("td");   
+ const country = document.createElement("td");
+ const number = document.createElement("td");
+ const email = document.createElement("td"); 
+ const profile = document.createElement("td");
+
+name.innerHTML = item.name;
+age.innerHTML = item.age;
+country.innerHTML = item.country;
+number.innerHTML = item.number;
+email.innerHTML = item.email;
+const image = document.createElement("img");
+image.setAttribute("src",item.image);
+
+profile.appendChild(image);
+
+
+ tr.appendChild(name);
+ tr.appendChild(age);
+ tr.appendChild(country);
+ tr.appendChild(number);
+ tr.appendChild(email);
+ tr.appendChild(profile);
+ table.appendChild(tr);
+ 
+
+ })
+});
